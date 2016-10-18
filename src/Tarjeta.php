6663e673ddd6ed27b01f045c40e57d7ecb2a8ca0
Joaquin <joaquinluismanuel@gmail.com>
2016-10-18 20:53:00 -0300

    Actualizacion

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -4,41 +4,78 @@ namespace Poli\Tarjeta;
 
 
 class Tarjeta implements Int_Tarjeta{
-	protected $saldo,$porcentaje;
-	protected $ultimafecha=0,$ultimabicipaga=0;
-	public $viajes;
+	protected $saldo,$porcentaje, $ultimafecha=0,$ultimabicipaga=0,$pasajesPlus=0,$tiempomaxtransbordo=3600, $viajes,$trans=0,$id;
+	protected $lunes, $dias = array(0 => "Lunes" , 1 => "Martes" , 2 => "Miercoles", 3 => "Jueves", 4 => "Viernes", 5 => "Sabado", 6 => "Domingo");
 
-	public function __construct (){
+	public function __construct ($id){
 		$this->saldo = 0;
 		$this->porcentaje = 1;
+		$this->lunes = strtotime("2016/01/04 00:00");
+		$this->id = $id;
 	}
 
-	public function pagar(Transporte $transporte, $fecha_y_hora){
+	public function pagar(Transporte $transporte, $fecha_y_hora){ 
+
+	//Para pagar Colectivos.
 
 	if($transporte->getTipo()==1){ 
 
 		$aux1 = strtotime($fecha_y_hora);
 		$aux2 = strtotime($this->ultimafecha);
 
-		if($this->ultimafecha == 0 || ($aux1-$aux2>3600) || $this->viajes[$this->ultimafecha]->getTransporte()->getId() == $transporte->getid()){ 
+		$dia = $aux1 - $this->lunes;
+		$a = $dia % 86400;
+		$dia = $dia - $a;
+		$dia = ($dia/86400) % 7;
+
+		if(($dia==5 && $a>50400 && $a<79200) || ($dia==6 && $a>21600 && $a<79200) || $a<21600 || $a>79200 ){$this->tiempomaxtransbordo=5400;} else {$this->tiempomaxtransbordo=3600;}
+
+		if($this->ultimafecha == 0 || ($aux1-$aux2>$this->tiempomaxtransbordo) || $this->viajes[$this->ultimafecha]->getTransporte()->getId() == $transporte->getid() || $this->trans==1){
 			$costo = ($transporte->getCosto()*$this->porcentaje);
+			$this->trans=0;
 		} else {
+
 			$costo = ($transporte->getCostoTrans()*$this->porcentaje);
+			$this->trans=1;
+
+			if($costo>$this->saldo){
+				$costo = ($transporte->getCosto()*$this->porcentaje);
+				$this->trans=0;
+			}
+
 		}
 
 
-		if($costo<=$this->saldo){
+		if($this->pasajesPlus<2){
+			if($costo>$this->saldo){
+				$this->pasajesPlus++;
+			}
+	
 			$this->saldo -= $costo;
 			$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
 			$this->ultimafecha = $fecha_y_hora;
-			return 1;
+
+
+			if($this->pasajesPlus!=0){
+				$boleto = new Boleto($fecha_y_hora,2,$costo,$this->saldo,$transporte->getId(),$this->id);
+			} else if ($this->porcentaje==0.5) {
+				$boleto = new Boleto($fecha_y_hora,3,$costo,$this->saldo,$transporte->getId(),$this->id);
+			} else {
+				$boleto = new Boleto($fecha_y_hora,1,$costo,$this->saldo,$transporte->getId(),$this->id);
+			}
+
+			// Se pudo pagar.
+			return $boleto;
 		} else {
+			// No se pudo pagar.
 			return 0;
 		}
 
 
 		} 
 
+
+	//Para pagar Bicis.
 
 	if($transporte->getTipo()==2){ 
 
@@ -56,10 +93,10 @@ class Tarjeta implements Int_Tarjeta{
 
 		$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
 		return 1;
+		} 
 
 	}
 
-	}
 
 	public function recargar($monto){
 		if($monto>=500){
@@ -68,6 +105,7 @@ class Tarjeta implements Int_Tarjeta{
 			$monto+=48;
 		}
 		$this->saldo+=$monto;
+		$this->pasajesPlus=0;
 	}
 
 	public function saldo(){
--- test/transbordoTest.php
+++ test/transbordoTest.php
@@ -4,47 +4,27 @@
 namespace Poli\Tarjeta;
 
 
-class TarjetaTest extends \PHPUnit_Framework_TestCase {
+class TransbordoTest extends \PHPUnit_Framework_TestCase {
 
   protected $tarjeta,$colectivoA,$colectivoB,$medio,$bici;	
 
   public function setup(){
-			$this->tarjeta = new Tarjeta(23);
+	  $this->tarjeta = new Tarjeta(23);
       $this->medio = new Medio();
-		  $this->colectivoA = new Colectivo("144 Negro", "Rosario Bus");
-  		$this->colectivoB = new Colectivo("135", "Rosario Bus");
+	  $this->colectivoA = new Colectivo("144 Negro", "Rosario Bus");
+  	  $this->colectivoB = new Colectivo("135", "Rosario Bus");
       $this->biciA = new Bicicleta("323");
       $this->biciB = new Bicicleta("111");
   }	
 
-  public function testCargaSaldo() {
-    $this->tarjeta->recargar(272);
-    $this->assertEquals($this->tarjeta->saldo(), 320, "Cuando cargo 272 deberia tener finalmente 320");
-    $this->tarjeta = new Tarjeta(45);
-    $this->tarjeta->recargar(505);
-    $this->assertEquals($this->tarjeta->saldo(), 645, "Cuando cargo 505 deberia tener finalmente 645");
-  }
-
-
-  public function testPagarViaje() {
-  	$this->tarjeta->recargar(272);
-    $this->assertEquals($this->tarjeta->pagar($this->colectivoA, "2016/06/30 22:50")->getTipo(),"Normal", "Cuando recargo y pago un colectivo deberia devolver un boleto de viaje Normal");
-  	$this->assertEquals($this->tarjeta->saldo(), 312, "Cuando recargo 272 y pago un colectivo deberia tener finalmente 312");
-  }
-
-
-  public function testPagarViajeSinSaldo() {
-  	$this->assertEquals($this->tarjeta->pagar($this->colectivoA, "2016/06/30 22:50")->getTipo(),"Plus", "Cuando no recargo y pago un colectivo deberia devolver un boleto de viaje plus");
-    $this->assertEquals($this->tarjeta->saldo(),-8, "Cuando no recargo y pago el saldo deberia ser -8");
-  }
-
-  /*
+  // Tomo 2 colectivos diferentes en menos de una hora.
   public function testTransbordo() {
   	$this->tarjeta->recargar(272);
   	$this->tarjeta->pagar($this->colectivoA, "2016/06/30 22:54");
   	$this->tarjeta->pagar($this->colectivoB, "2016/06/30 23:50");
   	$this->assertEquals($this->tarjeta->saldo(), 309.36, "Si tengo 312 y pago un colectivo con transbordo deberia tener finalmente 309.36");
   }
+
 
   public function testNoTransbordo() {
   	$this->tarjeta->recargar(272);
@@ -54,6 +34,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
  
   }
 
+  // Tomo el mismo colectivo en menos de una hora, no es Transbordo.
   public function testNoTransbordoMismoColectivo() {
   	$this->tarjeta->recargar(272);
   	$this->tarjeta->pagar($this->colectivoA, "2016/06/30 22:50");
@@ -61,7 +42,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
   	$this->assertEquals($this->tarjeta->saldo(), 304, "Si tengo 312 y pago un colectivo sin transbordo ya que es el mismo deberia tener finalmente 304");
   }
 
-
+  // Pago Transbordo con el Medio Boleto.
   public function testMedioTransbordo() {
     $this->medio->recargar(272);
     $this->medio->pagar($this->colectivoA, "2016/06/30 22:54");
@@ -69,45 +50,16 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($this->medio->saldo(), 314.68, "Si tengo 312 y pago un colectivo con transbordo deberia tener finalmente 314.68");
   }
 
-  
+
   public function testMedioNoTransbordo() {
     $this->medio->recargar(272);
     $this->medio->pagar($this->colectivoA, "2016/06/28 10:50");
-    $this->assertEquals($this->medio->pagar($this->colectivoB, "2016/06/30 23:58")->getTipo(),"Medio", "Cuando recargo y pago un colectivo con el medio deberia devolver un boleto de viaje Medio");
+    $this->medio->pagar($this->colectivoB, "2016/06/30 23:58");
     $this->assertEquals($this->medio->saldo(), 312, "Si tengo 312 y pago un colectivo sin transbordo deberia tener finalmente 312");
- 
-  }*/
-
-  public function testPagarBici() {
-    $this->tarjeta->recargar(272);
-    $this->tarjeta->pagar($this->biciA, "2016/06/30 22:54");
-    $this->assertEquals($this->tarjeta->saldo(), 308, "Si tengo 320 y pago una bici deberia tener finalmente 308");
   }
 
-  public function testPagarDosBiciUnDia() {
-    $this->tarjeta->recargar(272);
-    $this->tarjeta->pagar($this->biciA, "2016/06/30 10:54");
-    $this->tarjeta->pagar($this->biciB, "2016/06/30 22:54");
-    $this->assertEquals($this->tarjeta->saldo(), 308, "Si tengo 320 y pago una bici deberia tener finalmente 308");
-  }
 
-  public function testPagarDosBiciDosDias() {
-    $this->tarjeta->recargar(272);
-    $this->tarjeta->pagar($this->biciA, "2016/06/27 10:54");
-    $this->tarjeta->pagar($this->biciB, "2016/06/30 22:54");
-    $this->assertEquals($this->tarjeta->saldo(), 296, "Si tengo 320 y pago una bici deberia tener finalmente 308");
-  }
-
-  public function testViaje(){
-  	$this->tarjeta->recargar(272);
-  	$this->tarjeta->pagar($this->colectivoA, "2016/06/30 22:50");
-  	$this->assertEquals($this->tarjeta->viajesRealizados()["2016/06/30 22:50"]->getTipo(),"Viaje en Colectivo", "");
-  	$this->assertEquals($this->tarjeta->viajesRealizados()["2016/06/30 22:50"]->getHorario(),"2016/06/30 22:50", "");
-  	$this->assertEquals($this->tarjeta->viajesRealizados()["2016/06/30 22:50"]->getCosto(),8, "");
-  	$this->assertEquals($this->tarjeta->viajesRealizados()["2016/06/30 22:50"]->getTransporte()->getNombreEmpresa(),"Rosario Bus", "");
-  }
-
-  /*
+  // Tomo 3 colectivos en menos de una hora, solo uno de ellos es Transbordo.
   public function testUnTransbordoPorViaje(){
     $this->tarjeta->recargar(272);
     $this->tarjeta->pagar($this->colectivoA, "2016/06/30 22:54");
@@ -116,27 +68,54 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($this->tarjeta->saldo(), 301.36, "Si tengo 312 y pago un colectivo con transbordo y luego otro deberia tener finalmente 301.36");
   }
 
-
-   public function testTransbordo90min() {
+  // Pruebo transbordo antes de las 6.
+  public function testTransbordo90Madrugada() {
     $this->tarjeta->recargar(272);
     $this->tarjeta->pagar($this->colectivoA, "2016/10/18 4:10");
     $this->tarjeta->pagar($this->colectivoB, "2016/10/18 5:20");
     $this->assertEquals($this->tarjeta->saldo(), 309.36, "Si tengo 312 y pago un colectivo con transbordo deberia tener finalmente 309.36");
   }
 
-   public function testnoTransbordo90min() {
+  // Pruebo transbordo despues de las 22.
+  public function testTransbordo90Noche() {
+    $this->tarjeta->recargar(272);
+    $this->tarjeta->pagar($this->colectivoA, "2016/10/18 21:10");
+    $this->tarjeta->pagar($this->colectivoB, "2016/10/18 22:20");
+    $this->assertEquals($this->tarjeta->saldo(), 309.36, "Si tengo 312 y pago un colectivo con transbordo deberia tener finalmente 309.36");
+  }
+
+  // Pruebo que el tiempo max de transbordo un dia de semana de 6 a 22 no es 90 min.
+  public function testnoTransbordo90DiaSemana() {
     $this->tarjeta->recargar(272);
     $this->tarjeta->pagar($this->colectivoA, "2016/10/18 20:10");
     $this->tarjeta->pagar($this->colectivoB, "2016/10/18 21:20");
     $this->assertEquals($this->tarjeta->saldo(), 304, "Si tengo 312 y pago un colectivo con transbordo deberia tener finalmente 309.36");
   }
 
-   public function testTransbordo90sabado() {
+  // Tiempo max sabado de 14 a 22 es 90 min
+  public function testTransbordo90Sabado() {
     $this->tarjeta->recargar(272);
     $this->tarjeta->pagar($this->colectivoA, "2016/10/29 14:10");
     $this->tarjeta->pagar($this->colectivoB, "2016/10/29 15:20");
     $this->assertEquals($this->tarjeta->saldo(), 309.36, "Si tengo 312 y pago un colectivo con transbordo deberia tener finalmente 309.36");
-  }*/
+  }
+
+
+  // Tiempo max sabado de 6 a 22 es 90 min
+  public function testTransbordo90DomingoyFeriado() {
+    $this->tarjeta->recargar(272);
+    $this->tarjeta->pagar($this->colectivoA, "2016/10/30 14:10");
+    $this->tarjeta->pagar($this->colectivoB, "2016/10/30 15:20");
+    $this->assertEquals($this->tarjeta->saldo(), 309.36, "Si tengo 312 y pago un colectivo con transbordo deberia tener finalmente 309.36");
+  }
+
+  // Tiempo max sabado de 6 a 14 es 60 min
+  public function testNoTransbordo90Sabado() {
+    $this->tarjeta->recargar(272);
+    $this->tarjeta->pagar($this->colectivoA, "2016/10/29 8:10");
+    $this->tarjeta->pagar($this->colectivoB, "2016/10/29 9:20");
+    $this->assertEquals($this->tarjeta->saldo(), 304, "Si tengo 312 y pago un colectivo con transbordo deberia tener finalmente 309.36");
+  }
 
 }
 
